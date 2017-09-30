bc9b557627768fde510d10f5ec0db3f898f71c4d
Nadim <nadimmadani@gmail.com>
2017-09-30 09:26:22 -0500

    Add ideo list and pass props

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyA690eFFbZ2aj04F_0HBr4PbhyFG7TaX5I';
 
 class App extends Component {
@@ -9,7 +10,7 @@ class App extends Component {
 		super(props);
 		this.state = {videos: []};
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+		YTSearch({key: API_KEY, term: 'dogs'}, (videos) => {
 			this.setState({ videos });
 		});
 	}
@@ -18,7 +19,8 @@ class App extends Component {
 		return (
 			<div>
 				<SearchBar />
-			</div> 
+				<VideoList videos={this.state.videos}/>
+			</div>
 		);
 	}
 }
