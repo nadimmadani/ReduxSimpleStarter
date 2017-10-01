73f60954eb20fd7740cabfdf2495d2edb8b4e19e
Nadim <nadimmadani@gmail.com>
2017-10-01 10:56:03 -0500

    Added search functionality

--- src/index.js
+++ src/index.js
@@ -3,23 +3,36 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyA690eFFbZ2aj04F_0HBr4PbhyFG7TaX5I';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {videos: []};
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+		this.videoSearch('surfboards');
+	}
 
-		YTSearch({key: API_KEY, term: 'dogs'}, (videos) => {
-			this.setState({ videos });
+	videoSearch(term){
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 		});
 	}
 
 	render() {
 		return (
 			<div>
-				<SearchBar />
-				<VideoList videos={this.state.videos}/>
+				<SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos}/>
 			</div>
 		);
 	}
